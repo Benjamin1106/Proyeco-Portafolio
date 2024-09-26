38727f84f8a23e20d43acd3427bc7ff22b6f0e8b
braninostroza <bran.inostroza@duocuc.cl>
2024-09-26 17:28:19 -0300

    Jenkins problem result

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from './navbar.tsx';
 
 const Home: React.FC = () => <h1>Hola Mundo 1</h1>;
 const About: React.FC = () => <h1>Hola Mundo 2</h1>;
-const Services: React.FC = () => <h1>Hola Mundo 3</h1>>;
+const Services: React.FC = () => <h1>Hola Mundo 3</h1>;
 const Contact: React.FC = () => <h1>Hola Mundo 4</h1>;
 
 const App: React.FC = () => {
