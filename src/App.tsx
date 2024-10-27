ba4b98b9f95ed3c983c20196c563141b94feeb90
braninostroza <bran.inostroza@duocuc.cl>
2024-10-27 13:34:09 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Componentes/navbar.tsx';
 import FormularioSolicitudes from './Componentes/solicitudes.tsx';
 
-const Inicio: React.FC = () => <h1>Hola Mundo 1</h1>;
+const Inicio: React.FC = () => <h1>Hola Juan Saludos</h1>;
 const Services: React.FC = () => <h1>Hola Mundo 3</h1>;
 const Contact: React.FC = () => <h1>Hola Mundo 4</h1>;
 
