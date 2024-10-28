c86d3549e9f6704083f440f89afa083a055c42ed
braninostroza <bran.inostroza@duocuc.cl>
2024-10-27 23:39:34 -0300

    footer v2

--- src/Componentes/footer.tsx
+++ src/Componentes/footer.tsx
@@ -6,18 +6,18 @@ const Footer: React.FC = () => {
     <footer className="footer">
       <div className="footer-content">
         <div className="footer-section footer-links">
-          <h4>Enlaces Rápidos</h4>
+          <h4>Fonos de Emergencia </h4>
           <ul>
-            <li><a href="#about">¿Quiénes Somos?</a></li>
-            <li><a href="#contact">Contacto</a></li>
-            <li><a href="#services">Servicios</a></li>
+            <li>Fono 1</li>
+            <li>Fono 2</li>
+            <li>Fono 3</li>
           </ul>
         </div>
         <div className="footer-section footer-contact">
           <h4>Contacto</h4>
-          <p>Villa Los Lagos, Chile</p>
-          <p>Teléfono: +56 9 1234 5678</p>
-          <p>Email: contacto@juntaloslagos.cl</p>
+          <p className='test'>Villa Los Lagos, Chile</p>
+          <p className='test'>Teléfono: +56 9 1234 5678</p>
+          <p className='test'>Email: contacto@juntaloslagos.cl</p>
         </div>
         <div className="footer-section footer-social">
           <h4>Síguenos</h4>
@@ -27,7 +27,7 @@ const Footer: React.FC = () => {
         </div>
       </div>
       <div className="footer-bottom">
-        <p>&copy; 2024 Junta de Vecinos. Todos los derechos reservados.</p>
+        <p className='test'>&copy; 2024 Junta de Vecinos. Todos los derechos reservados.</p>
       </div>
     </footer>
   );
