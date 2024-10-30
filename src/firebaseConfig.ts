8e22b07c9a91d7220240c2e7ec4cb3ffd3c5178c
braninostroza <bran.inostroza@duocuc.cl>
2024-10-29 21:25:27 -0300

    test firestore

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
+// Configuración de Firebase
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -16,4 +17,18 @@ const app = initializeApp(firebaseConfig);
 // Inicializar Firestore
 const db = getFirestore(app);
 
+// Función para agregar un documento a Firestore
+const agregarDocumentoPrueba = async () => {
+  try {
+    await addDoc(collection(db, 'testCollection'), { campoPrueba: 'valorPrueba' });
+    console.log('¡Documento de prueba agregado!');
+  } catch (error) {
+    console.error('Error al agregar documento de prueba:', error);
+  }
+};
+
+// Llama a la función al iniciar la aplicación
+agregarDocumentoPrueba();
+
 export { db };
+
