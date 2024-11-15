af2511d4f9bdd4276f4adf63dbcd5c66fe572288
Ruben Ramirez Ross <101893112+RubenRRoss@users.noreply.github.com>
2024-11-15 19:37:55 -0300

    act footer

--- src/Componentes/footer.tsx
+++ src/Componentes/footer.tsx
@@ -15,16 +15,16 @@ const Footer: React.FC = () => {
         <div className="footer-section footer-num">
           <h4>Fonos de Emergencia</h4>
           <ul>
-            <li><a href="tel:+56912345678">Carabineros: 133</a></li>
-            <li><a href="tel:+56987654321">Bomberos: 132</a></li>
-            <li><a href="tel:+56911223344">Ambulancia: 131</a></li>
+            <li><a href="tel:133">Carabineros: 133</a></li>
+            <li><a href="tel:132">Bomberos: 132</a></li>
+            <li><a href="tel:131">Ambulancia: 131</a></li>
           </ul>
         </div>
         
         <div className="footer-section footer-contact">
           <h4>Contacto</h4>
           <p>Villa Los Lagos, Chile</p>
-          <p>Teléfono: <a href="tel:+56999887766">+56 9 9988 7766</a></p>
+          <p>Teléfono: <a href="tel:+56975131688">+56 9 7513 1688</a></p>
           <p>Email: <a href="mailto:contacto@juntaloslagos.cl">contacto@juntaloslagos.cl</a></p>
         </div>
         
