855b4ed7a8c53889d616da4c648990ceb3098151
Ruben Ramirez Ross <101893112+RubenRRoss@users.noreply.github.com>
2024-11-15 00:01:10 -0300

    Update firebaseConfig.ts

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
+import { getAnalytics } from "firebase/analytics";
 // Configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBSNuhyl2g56W6AbpdVZOEcH33twrpUiE0",
@@ -9,6 +9,7 @@ const firebaseConfig = {
   storageBucket: "proyecto-de-titulo-e4070.appspot.com",
   messagingSenderId: "792762932008",
   appId: "1:792762932008:web:135a10e5c85e04f0020e1d",
+  measurementId: "G-FN5CE2V6BE"
 };
 
 // Inicializar Firebase
@@ -16,6 +17,8 @@ const app = initializeApp(firebaseConfig);
 
 // Inicializar Firestore
 const db = getFirestore(app);
+
+const analytics = getAnalytics(app);
 
 // Exportar la instancia de Firestore
 export { db };
