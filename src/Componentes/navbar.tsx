1a3ea04a85e70115bcddb888be8541c07b2f2396
braninostroza <bran.inostroza@duocuc.cl>
2024-11-18 16:18:56 -0300

    Inngresa boton login

--- src/Componentes/navbar.tsx
+++ src/Componentes/navbar.tsx
@@ -128,7 +128,7 @@ const Navbar: React.FC<NavbarProps> = ({ isAuthenticated, setIsAuthenticated, ro
               </button>
             ) : (
               <button className="navbar-button" onClick={openModal}>
-                <FaSignInAlt className="navbar-icon" />Conectate
+                <FaSignInAlt className="navbar-icon" />Ingresa
               </button>
             )}
           </div>
