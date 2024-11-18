19524f105daa586aae3cfc20415a9ece8520af72
braninostroza <bran.inostroza@duocuc.cl>
2024-11-17 22:32:10 -0300

    sin link

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './Componentes/navbar';
 import FormularioSolicitudes from './Componentes/solicitudes';
 import Home from './Componentes/home';
