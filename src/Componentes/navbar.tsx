7863bdb9d16b0290b7dd5d8bf538f960e9a5663f
Ruben Ramirez Ross <101893112+RubenRRoss@users.noreply.github.com>
2024-12-06 19:09:36 -0300

    Update navbar.tsx

--- src/Componentes/navbar.tsx
+++ src/Componentes/navbar.tsx
@@ -58,20 +58,6 @@ const Navbar: React.FC<NavbarProps> = ({ isAuthenticated, setIsAuthenticated, ro
     localStorage.removeItem('userNombre');
   }, [setIsAuthenticated, setRole]);
 
-  // Calculamos el número de enlaces en el menú
-  const getMenuItemsCount = () => {
-    let count = 3; // Siempre hay al menos 3 enlaces por defecto
-
-    // Agregar elementos adicionales dependiendo de si está autenticado o no
-    if (isAuthenticated) {
-      count += role === 'directiva'
-        ? 5 // Si es directiva, se muestran 5 enlaces más
-        : 3; // Si es vecino, se muestran 3 enlaces más
-    }
-
-    return count;
-  };
-
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -79,23 +65,25 @@ const Navbar: React.FC<NavbarProps> = ({ isAuthenticated, setIsAuthenticated, ro
           <img src={logo} alt="Logo Junta de Vecinos" className="logo-image" />
           Junta de Vecinos
         </Link>
-
-        {getMenuItemsCount() > 3 && (
-          <div className="menu-icon" onClick={toggleMenu}>
-            &#9776;
-          </div>
-        )}
-
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776;
+        </div>
         <ul className={`nav-menu ${isActive ? 'active' : ''}`}>
           <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={toggleMenu}>Inicio</Link>
+            <Link to="/" className="nav-links" onClick={toggleMenu}>
+              Inicio
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/noticias" className="nav-links" onClick={toggleMenu}>Noticias</Link>
+            <Link to="/noticias" className="nav-links" onClick={toggleMenu}>
+              Noticias
+            </Link>
           </li>
           {role !== 'directiva' && (
             <li className="nav-item">
-              <Link to="/contacto" className="nav-links" onClick={toggleMenu}>Contacto</Link>
+              <Link to="/contacto" className="nav-links" onClick={toggleMenu}>
+                Contacto
+              </Link>
             </li>
           )}
           {isAuthenticated && (
@@ -103,48 +91,61 @@ const Navbar: React.FC<NavbarProps> = ({ isAuthenticated, setIsAuthenticated, ro
               {role === 'directiva' && (
                 <>
                   <li className="nav-item">
-                    <Link to="/crearActividades" className="nav-links" onClick={toggleMenu}>Crear</Link>
+                    <Link to="/crearActividades" className="nav-links" onClick={toggleMenu}>
+                      Crear
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/lup" className="nav-links" onClick={toggleMenu}>Lup</Link>
+                    <Link to="/lup" className="nav-links" onClick={toggleMenu}>
+                      Lup
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/usersList" className="nav-links" onClick={toggleMenu}>Usuarios</Link>
+                    <Link to="/usersList" className="nav-links" onClick={toggleMenu}>
+                      Usuarios
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/grafico" className="nav-links" onClick={toggleMenu}><FaChartBar /></Link>
+                    <Link to="/grafico" className="nav-links" onClick={toggleMenu}>
+                      <FaChartBar />
+                    </Link>
                   </li>
-
                 </>
               )}
               {role === 'vecino' && (
                 <>
                   <li className="nav-item">
-                    <Link to="/solicitudes" className="nav-links" onClick={toggleMenu}>Solicitudes</Link>
+                    <Link to="/solicitudes" className="nav-links" onClick={toggleMenu}>
+                      Solicitudes
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/actividades" className="nav-links" onClick={toggleMenu}>Participa</Link>
+                    <Link to="/actividades" className="nav-links" onClick={toggleMenu}>
+                      Participa
+                    </Link>
                   </li>
                   <li className="nav-item">
-                    <Link to="/videos" className="nav-links" onClick={toggleMenu}>Videos Tutoriales</Link>
+                    <Link to="/videos" className="nav-links" onClick={toggleMenu}>
+                      Videos Tutoriales
+                    </Link>
                   </li>
                 </>
               )}
             </>
           )}
           {isAuthenticated && (
-            <li className="nav-item user-name">
-              Usuario: {userName}
-            </li>
+            <li className="nav-item user-name">Usuario: {userName}</li>
           )}
           <div className="navbar-button-container">
             {isAuthenticated ? (
               <button className="navbar-button-logout" onClick={handleLogout}>
-                <FaSignOutAlt className="navbar-icon" />Salir
+                <FaSignOutAlt className="navbar-icon" />
+                Salir
               </button>
             ) : (
               <button className="navbar-button" onClick={openModal}>
-                <FaSignInAlt className="navbar-icon" />Ingresa
+                <FaSignInAlt className="navbar-icon" />
+                Ingresa
               </button>
             )}
           </div>
